436c285b360ac633cff7ebb99470bb67d7562994
Vijay Mahes <Vijaypradhap2004@gmail.com>
2026-08-07 09:03:05 +0530

    fix: Update MainActivity and AndroidManifest for Flutter embedding v2 and safe navigation

--- android/app/src/main/java/ac/ruraluniv/gri/MainActivity.kt
+++ android/app/src/main/java/ac/ruraluniv/gri/MainActivity.kt
@@ -1,29 +1,6 @@
 package ac.ruraluniv.gri
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import ac.ruraluniv.gri.navigation.SetupNavGraph
-import ac.ruraluniv.gri.ui.theme.GriTheme
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            GriTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    SetupNavGraph(navController = navController)
-                }
-            }
-        }
-    }
+class MainActivity: FlutterActivity() {
 }
